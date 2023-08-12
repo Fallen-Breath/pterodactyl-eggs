b3591a768cd3b5517cbc0ea23e6c83b3fa6e99d0
Fallen_Breath <yqx791125833@gmail.com>
2023-08-13 03:21:45 +0800

    splits non-slim images

--- image-main/build.sh
+++ image-main/build.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -e
 
-for JAVA in 8 17; do
-  for MCDR in "2.10" "latest"; do
-    if [ "$MCDR" == "latest" ]; then
-      MCDR_REQUIREMENT="mcdreforged"
-    else
-      MCDR_REQUIREMENT="mcdreforged~=${MCDR}"
-    fi
-    TAG="fallenbreath/pterodactyl-mc-mcdr:bullseye-${JAVA}-${MCDR}"
-    echo "======== Java: $JAVA, MCDR: $MCDR, Tag: $TAG ========"
+for SYS in "bullseye" "slim-bullseye"; do
+  for JAVA in 8 17; do
+    for MCDR in "2.10" "latest"; do
+      if [ "$MCDR" == "latest" ]; then
+        MCDR_REQUIREMENT="mcdreforged"
+      else
+        MCDR_REQUIREMENT="mcdreforged~=${MCDR}"
+      fi
+      TAG="fallenbreath/pterodactyl-mc-mcdr:${SYS}-${JAVA}-${MCDR}"
+      echo "======== System: $SYS, Java: $JAVA, MCDR: $MCDR, Tag: $TAG ========"
 
-    docker build "$(pwd)" \
-      --build-arg JAVA_VERSION=${JAVA} \
-      --build-arg MCDR_REQUIREMENT=${MCDR_REQUIREMENT} \
-      -t "$TAG"
+      docker build "$(pwd)" \
+        --build-arg SYSTEM=${SYS} \
+        --build-arg JAVA_VERSION=${JAVA} \
+        --build-arg MCDR_REQUIREMENT=${MCDR_REQUIREMENT} \
+        -t "$TAG"
 
-    if [ $# == 1 ] && [ "$1" == "-p" ]; then
-      docker push $TAG
-    fi
+      if [ $# == 1 ] && [ "$1" == "-p" ]; then
+        docker push $TAG
+      fi
+    done
   done
 done
